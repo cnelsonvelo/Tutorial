b6038ba678e92d5380a0d57380efc8001935a5a2
cnelsonvelo <cnelsonvelo@gmail.com>
2013-12-12 08:58:35 -0800

    Update action bar

--- FirstApp/src/com/example/firstapp/MainActivity.java
+++ FirstApp/src/com/example/firstapp/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.firstapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -25,6 +27,28 @@ public class MainActivity extends Activity {
         return true;
     }
     
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		case R.id.action_search:
+			openSearch();
+			return true;
+		case R.id.action_settings:
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+    
     public void sendMessage(View view) {
     	Intent intent = new Intent(this, DisplayMessageActivity.class);
     	EditText txt = (EditText) findViewById(R.id.edit_text);
@@ -33,4 +57,8 @@ public class MainActivity extends Activity {
     	startActivity(intent);
     }
     
+    private void openSearch() {
+    	Intent intent = new Intent(this, SearchActivity.class);
+    	startActivity(intent);
+    }
 }
